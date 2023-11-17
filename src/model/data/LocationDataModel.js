388ca1d11c8fac824b474591aeba2808f72e52a1
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-11-17 20:06:16 +0300

    Create LocationDataModel.js

---
+++ src/model/data/LocationDataModel.js
@@ -0,0 +1,11 @@
+let LocationDataModel = function (
+    country,
+    geoName,
+    locations,
+    imageUrl) {
+    this.country = country;
+    this.geoName = geoName;
+    this.locations = locations;
+    this.imageUrl = imageUrl;
+}
+module.exports = LocationDataModel;
