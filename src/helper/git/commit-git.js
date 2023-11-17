a06cebf61af44545413b1010ff43d1122a7661f9
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-11-18 01:57:18 +0300

    Update commit-git.js

--- src/helper/git/commit-git.js
+++ src/helper/git/commit-git.js
@@ -1,7 +1,7 @@
 const git = require('../../core/git');
 let commitGit = function () {
-    let INSIGHT_BOT_USERNAME = 'akramghaleb';
-    let INSIGHT_BOT_EMAIL = 'akramghammari@gmail.com';
+    let INSIGHT_BOT_USERNAME = 'github-actions[bot]';
+    let INSIGHT_BOT_EMAIL = '41898282+github-actions[bot]@users.noreply.github.com';
     let commit = async function (message) {
         console.log(`Git Commit "${message}"`)
         try {
