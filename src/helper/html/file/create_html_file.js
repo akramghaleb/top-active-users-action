0ef782f4916427e093ddd795aa985b02e64e15e0
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-11-17 20:05:22 +0300

    Create create_html_file.js

---
+++ src/helper/html/file/create_html_file.js
@@ -0,0 +1,39 @@
+let createHtmlFile = (function () {
+    let create = function () {
+        let html = `<!DOCTYPE html>\n`;
+        html = html + `<html>\n`;
+        html = html + `\t<head>\n`;
+        html = html + `\t\t<title>Total Public Contributions in GitHub by Country</title>\n`;
+        html = html + `\t\t<link rel="stylesheet" href="./css/styles.css">\n`;
+        html = html + `\t\t<script src="./javascript/chart.min.js"></script>\n`;
+        html = html + `\t\t<script src="./javascript/index.umd.min.js"></script>\n`;
+        html = html + `\t\t<script src="./javascript/graph.js"></script>\n`;
+        html = html + `\t\t<script src="./javascript/resizeCanvas.js"></script>\n`;
+        html = html + `\t</head>\n`;
+        html = html + `\t<body>\n`;
+        html = html + `\t<div class="header">\n`;
+        html = html + `\t\t<div class="description">\n`;
+        html = html + `\t\t\t<strong>Total Public Contributions in GitHub by Country</strong>\n`;
+        html = html + `\t\t</div>\n`;
+        html = html + `\t</div>\n`;
+        html = html + `\t<div class="canvas">\n`;
+        html = html + `\t\t<canvas id="canvas"></canvas>\n`;
+        html = html + `\t</div>\n`;
+        html = html + `\t<div class="footer">\n`;
+        html = html + `\t\t<div class="description">\n`;
+        html = html + `\t\t\tList of most active GitHub users based on public contributions by country.\n`;
+        html = html + `\t\t\tGo to repository <a href="https://github.com/akramghaleb/top-active-users">akramghaleb/top-active-users</a>\n`;
+        html = html + `\t\t</div>\n`;
+        html = html + `\t</div>\n`;
+        html = html + `\t<script type="application/javascript">\n`;
+        html = html + `\t\tresizeCanvas()\n`;
+        html = html + `\t</script>\n`;
+        html = html + `\t</body>\n`;
+        html = html + `</html>`
+        return html;
+    }
+    return {
+        create: create,
+    };
+})();
+module.exports = createHtmlFile;
